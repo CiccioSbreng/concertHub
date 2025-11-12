c623d0c0996d1382714e2a33d0414252a1a1c823
Fabio Annoni <boc_flub@hotmail.it>
2025-11-13 00:08:14 +0100

    niente di nuovo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 
-export default function App(){
-  return <Home />
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
